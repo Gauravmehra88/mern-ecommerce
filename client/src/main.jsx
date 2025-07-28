5cbd9b92405c5c28775877f7cf7ebc3285aaed28
Gaurav Mehra <gauravmehra5263@gmail.com>
2025-07-28 08:49:58 +0530

    react-router and redux setup is done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'  // <--- This must be present
+import store from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//wrap our app in browser router
+<BrowserRouter>
+     <Provider store={store}>
+         <App />
+     </Provider>
+  </BrowserRouter>
+
 )
 
