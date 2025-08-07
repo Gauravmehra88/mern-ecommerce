a7d813a9efc829da53921f96329738cfc7e3870c
Gaurav Mehra <gauravmehra5263@gmail.com>
2025-08-07 17:24:29 +0530

    form .jsx update

--- client/src/components/common/form.jsx
+++ client/src/components/common/form.jsx
@@ -1,17 +1,64 @@
-const { Label } = require("@radix-ui/react-label");
+import { SelectContent, SelectItem, SelectValue } from "@radix-ui/react-select";
 
-function CommonForm(formControls) {
+const { Label } = require("@radix-ui/react-label");
+const { Input } = require("postcss");
+
+function CommonForm({ formControls, formData , setFormData ,onSubmit , buttonText }) {
     
     function renderInputsByComponentsType(getcontrolItem){
         let element=null;
         switch (getcontrolItem.componentType) {
-            case value:
-                
+            case 'input':
+                element = (
+                <Input
+               name={getcontrolItem.name}
+               placeholder={getcontrolItem.placeholder}
+               id={getcontrolItem.name}
+               type={getcontrolItem.type}
+                />
+        );
                 break;
+                     case 'select':
+                element = (
+               <select>
+                <selectTrigger className="w-full ">
+                    <SelectValue placeholder={getcontrolItem.placeholder}/>
+
+                </selectTrigger>
+                <SelectContent>
+                    {
+                        getcontrolItem.options && 
+                        getcontrolItem.options.length > 0 ?
+                        getcontrolItem.map(optionItem=> <SelectItem key={optionItem.id} value={optionItem.id}>{optionItem.label}</SelectItem>) : null
+                    }
+                </SelectContent>
+               </select>
+        );
+                break;
+                     case 'textarea':
+                element = (
+               <Textarea
+               name={getcontrolItem.name}
+               placeholder={getcontrolItem.placeholder}
+               id={getcontrolItem.id}
+               />
+        );
+                break;
+                
         
             default:
-                break;
-        }
+                 element = (
+                <Input
+               name={getcontrolItem.name}
+               placeholder={getcontrolItem.placeholder}
+               id={getcontrolItem.name}
+               type={getcontrolItem.type}
+                />
+        );
+      break;
+        } 
+        return element
+    }
     return(
         <form >
             <div className="flex flex-col gap-3">
